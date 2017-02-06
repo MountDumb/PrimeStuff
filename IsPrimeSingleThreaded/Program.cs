d43036dc9bddd8088d754689375b9b2c5c6a2caa
UrbanSkurk <UrbanSkurk@VARMEVRK>
2017-02-06 13:32:45 +0100

    Add project files.

---
+++ IsPrimeSingleThreaded/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+namespace IsPrimeSingleThreaded
+{
+    class Program
+    {
+        bool goOn = true;
+
+        static void Main(string[] args)
+        {
+            Program program = new IsPrimeSingleThreaded.Program();
+            program.Run();
+        }
+
+        public void Run()
+        {
+            do
+            {
+                long input = GetUserValue();
+                if (input == 0)
+                {
+                    goOn = false;
+                }
+                else
+                {
+                    PrimeChecker pc = new PrimeChecker(input);
+                    Thread t = new Thread(new ThreadStart(pc.Check));
+                    t.IsBackground = true;
+                    t.Start();
+
+                    //pc.Check();
+                }
+            } while (goOn);
+        }
+
+        private long GetUserValue()
+        {
+            Console.Write("Enter a positive integer value to test whether it is prime (enter 0 to exit) : ");
+            return long.Parse(Console.ReadLine());
+        }
+
+
+
+    }
+
+
+        
+    
+}
---
+++ IsPrimeSingleThreaded/ThreadMan.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace IsPrimeSingleThreaded
+{
+    public class ThreadMan
+    {
+        //private const int threads = 3;
+        //private const int workitems = 20;
+        //private static Object locker = new Object();
+        //static void Worker()
+        //{
+        //    while (true)
+        //    {
+        //        lock (locker)
+        //        {
+        //            Monitor.Wait(locker);
+        //        }
+        //        System.Console.WriteLine("{0} access the website c-sharpcorner", Thread.CurrentThread.Name);
+        //        Thread.Sleep(100);
+        //    }
+        //}
+        //[STAThread]
+        //static void Main(string[] args)
+        //{
+        //    Thread[] t = new Thread[threads];
+        //    for (int k = 0; k < threads; k++)
+        //    {
+        //        t[k] = new Thread(new ThreadStart(Worker));
+        //        t[k].Name = "user " + k;
+        //        t[k].IsBackground = true;
+        //        t[k].Start();
+                
+        //    }
+        //    for (int i = 0; i < workitems; i++)
+        //    {
+        //        Thread.Sleep(1000);
+        //        lock (locker)
+        //        {
+        //            Monitor.Pulse(locker);
+        //        }
+        //    }
+        //    Console.Read();
+        //}
+    }
+}
